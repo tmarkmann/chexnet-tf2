7dae3963f47dcb36f3d1683f11098aab7d08204f
tmarkmann <tmarkmann@techfak.uni-bielefeld.de>
2021-11-16 17:34:15 +0100

    Set download data in load_datasets to true

--- chexnet/scripts/load_dataset.py
+++ chexnet/scripts/load_dataset.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from chexnet.dataloader.cxr14_dataset import CXR14Dataset
 from chexnet.configs.config import chexnet_config
 
-chexnet_config["dataset"]["download"] = False
+chexnet_config["dataset"]["download"] = True
 #chexnet_config["dataset"]["data_dir"] = "~/tf_datasets"
 
 dataset = CXR14Dataset(chexnet_config)
