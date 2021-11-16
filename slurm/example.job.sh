39b13edc45068b1dcb7e03e233404f6834392b7d
tmarkmann <tmarkmann@techfak.uni-bielefeld.de>
2021-11-16 17:23:48 +0100

    Fix conda source in slurm job

--- slurm/example.job.sh
+++ slurm/example.job.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+source /media/compute/homes/tmarkmann/miniconda3/etc/profile.d/conda.sh
 conda activate chexnet
+
 python3 -c "import tensorflow as tf; print('Num GPUs Available: ', len(tf.config.experimental.list_physical_devices('GPU')))"
