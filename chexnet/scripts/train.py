6c8aa4ce05313951d20d417dd5d6bf65659d23f8
tmarkmann <tmarkmann@techfak.uni-bielefeld.de>
2021-11-18 19:20:58 +0100

    Add tensorboard callbacks to train

--- chexnet/scripts/train.py
+++ chexnet/scripts/train.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import tensorflow as tf
+from datetime import datetime
 from chexnet.dataloader.cxr14_dataset import CXR14Dataset
 from chexnet.model.chexnet import CheXNet
 from chexnet.configs.config import chexnet_config
@@ -19,10 +20,13 @@ model.compile(
     metrics=[tf.keras.metrics.AUC(curve='ROC',multi_label=True, num_labels=len(chexnet_config["data"]["class_names"]), from_logits=False)],
 )
 
-model.summary()
+log_dir = 'logs/chexnet/'+ datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
 
 model.fit(
     dataset.ds_train,
     epochs=chexnet_config["train"]["epochs"],
     validation_data=dataset.ds_test,
+    callbacks=[tensorboard_callback]
 )
