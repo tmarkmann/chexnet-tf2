dd1ee3ba51586f4af5ea4b218cb9ca2f9750bd1c
tmarkmann <tmarkmann@techfak.uni-bielefeld.de>
2021-12-07 14:45:41 +0100

    Add additional slurm scripts

---
+++ slurm/train_kaggle_3.job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /media/compute/homes/tmarkmann/miniconda3/etc/profile.d/conda.sh
+conda activate chexnet
+cd /media/compute/homes/tmarkmann/chexnet-tf2
+
+python3 -m chexnet.scripts.train_kaggle --augmentation --use_chexnet_weights
